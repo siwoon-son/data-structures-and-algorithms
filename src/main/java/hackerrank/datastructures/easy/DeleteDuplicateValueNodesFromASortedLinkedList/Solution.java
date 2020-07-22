64e4d4d2667bde8f9796401dfe21b63fedd4915e
siwoon-son <siwoon.son@gmail.com>
2020-07-22 21:20:46 +0900

    Newly solved problems!

---
+++ src/main/java/hackerrank/datastructures/easy/DeleteDuplicateValueNodesFromASortedLinkedList/Solution.java
@@ -0,0 +1,125 @@
+package hackerrank.datastructures.easy.DeleteDuplicateValueNodesFromASortedLinkedList;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the removeDuplicates function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head) {
+        // 정렬된 연결리스트에서 중복값을 제거한다.
+        // 연결리스트의 길이가 1 이상이므로,
+        // prev를 첫 번째 노드로, curr를 두 번째 노드로 지정한다.
+        SinglyLinkedListNode prev = head;
+        SinglyLinkedListNode curr = head.next;
+
+        // curr가 null일 때까지 반복한다.
+        while (curr != null) {
+            // 이전 값과 현재 값이 같으면,
+            if (prev.data == curr.data) {
+                // 이전 노드의 다음을 현재 노드의 다음으로 바꾼다.
+                prev.next = curr.next;
+            }
+            // 이전 값과 현재 값이 다르면,
+            else {
+                // 이전 노드를 현재 노드로 한 칸 이동한다.
+                prev = prev.next;
+            }
+            // 현재 노드를 다음 노드로 한 칸 이동한다.
+            curr = curr.next;
+        }
+
+        return head;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            SinglyLinkedListNode llist1 = removeDuplicates(llist.head);
+
+            printSinglyLinkedList(llist1, " ", bufferedWriter);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
