0531d35f6620ed3294f1aa638f77a478b8c9861d
siwoon-son <siwoon.son@gmail.com>
2020-08-29 00:33:37 +0900

    Newly solved problem.

--- src/main/java/hackerrank/algorithms/medium/ThePowerSum/Solution.java
+++ src/main/java/hackerrank/algorithms/medium/ThePowerSum/Solution.java
@@ -1,12 +1,7 @@
-package hackerrank.algorithms.easy.ThePowerSum;
+package hackerrank.algorithms.medium.ThePowerSum;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class Solution {
 
